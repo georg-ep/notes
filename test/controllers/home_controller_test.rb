4745cad607831d9597bdb820dd7b7d60dec064e6
gp00482 <gp00482@surrey.ac.uk>
2020-10-25 17:12:09 +0000

    Added Testing

---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get root_url
+    assert_response :success
+    assert_select 'title', 'My Notes'
+    assert_select 'h1', 'My Notes'
+  end
+
+end
